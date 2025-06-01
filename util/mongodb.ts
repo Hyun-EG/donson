c7905dc01d8eb2a8b682c1de89274a4697ae8183
Hyun-EG <codiee@naver.com>
2025-06-01 18:12:48 +0900

    Feat: 몽고디비 연결 로직 추가

---
+++ util/mongodb.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb";
+
+const url = `mongodb+srv://donson:${process.env.NEXT_PUBLIC_MONGO_PASSWORD}@donson.ud1ok3i.mongodb.net/?retryWrites=true&w=majority&appName=donson`;
+const client = new MongoClient(url);
+let connectDB: Promise<MongoClient>;
+
+declare global {
+  var _mongo: Promise<MongoClient> | undefined;
+}
+
+if (!global._mongo) global._mongo = client.connect();
+connectDB = global._mongo;
+
+export { connectDB };
