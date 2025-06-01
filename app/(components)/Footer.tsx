2b3df7949c452fb4979cdb3b785237a8517613d0
seonghyun <codiee@naver.com>
2025-06-02 00:18:58 +0900

    Add: 푸터 추가 및 인포페이지, 공지사항페이지 레이아웃 반영

---
+++ app/(components)/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section className="fixed bottom-0 left-0 w-full h-14 flex flex-col justify-center items-center border-t border-[#bebebe]">
+      <p className="font-bold text-sm">Data based on NEXON Open API</p>
+    </section>
+  );
+};
+
+export default Footer;
