805bd49956c23eb3abba4d3646b2dc00453bb6e0
Hyun-EG <codiee@naver.com>
2025-06-01 18:12:30 +0900

    Feat: 몽고디비 연결 및 로그인 기능 컨펌

---
+++ app/api/signin/route.ts
@@ -0,0 +1,47 @@
+import { connectDB } from "@/util/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+export async function POST(req: NextRequest) {
+  const data = await req.formData();
+  const id = data.get("id")?.toString();
+  const password = data.get("password")?.toString();
+  const db = (await connectDB).db("donson");
+  if (!id || !password) {
+    return NextResponse.json(
+      { message: "모든 필드를 입력해주세요", status: 400 },
+      { status: 400 }
+    );
+  }
+
+  const user = await db.collection("user").findOne({
+    $or: [{ id }],
+  });
+  if (!user) {
+    return NextResponse.json(
+      { message: "유저 정보가 존재하지 않습니다", status: 400 },
+      { status: 400 }
+    );
+  }
+
+  const isMatch = await bcrypt.compare(password, user.hashedPW);
+
+  try {
+    if (!isMatch) {
+      return NextResponse.json(
+        { message: "비밀번호가 일치하지 않습니다", status: 400 },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json(
+      { message: "로그인에 성공하였습니다.", status: 200 },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("서버 에러입니다.", error);
+    return NextResponse.json(
+      { message: "서버 오류", status: 500 },
+      { status: 500 }
+    );
+  }
+}
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const SignIn = () => {
   return (
     <section className="w-full min-h-screen py-8 px-8 flex flex-col items-center">
@@ -5,19 +7,25 @@ const SignIn = () => {
         <span className="text-3xl font-bold">DONSON</span>
       </header>
       <main className="w-full">
-        <form className="flex flex-col gap-2" action="/api/login" method="POST">
+        <form
+          className="flex flex-col gap-2"
+          action="/api/signin"
+          method="POST"
+        >
           <input
+            name="id"
             className="w-full h-10 px-2 border border-[#bebebe] bg-white focus:ring-2 focus:ring-sky-500 focus:outline-none"
             type="text"
             placeholder="아이디를 입력해주세요."
           />
           <input
+            name="password"
             className="w-full h-10 px-2 border border-[#bebebe] bg-white focus:ring-2 focus:ring-sky-500 focus:outline-none"
-            type="text"
+            type="password"
             placeholder="비밀번호를 입력해주세요."
           />
           <button
-            className="w-full h-10 bg-[#bebebe] text-white rounded-xl"
+            className="w-full h-10 bg-sky-500 text-white rounded-xl"
             type="submit"
           >
             로그인
@@ -26,7 +34,9 @@ const SignIn = () => {
         <article className="py-4">
           <p className="text-center">
             아직 회원이 아니신가요?
-            <span className="text-sky-500"> 회원가입 하러가기</span>
+            <Link href="/signup" className="text-sky-500">
+              회원가입 하러가기
+            </Link>
           </p>
         </article>
       </main>
