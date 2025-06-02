092a1b8dca5ed33e6ab02f2a6ce60bdcd699494f
Hyun-EG <codiee@naver.com>
2025-06-02 20:07:27 +0900

    Feat: OCID 가져오는 API 추가

--- app/api/get-char-ocid/route.ts
+++ app/api/get-char-ocid/route.ts
@@ -1,49 +1,19 @@
 import { connectDB } from "@/util/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
-const API_KEY = `${process.env.NEXT_PUBLIC_NEXON_API_KEY}`;
-
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const { userId } = body;
-  try {
-    if (body) {
-      const db = (await connectDB).db("donson");
-      const user = await db.collection("user").findOne({ userId: userId });
-      if (!user) {
-        throw new Error("사용자를 찾을 수 없습니다.");
-      }
-      const charName = user.charName;
-      console.log(charName);
-      const characterName = charName;
-      const urlString =
-        "https://open.api.nexon.com/maplestory/v1/id?character_name=" +
-        characterName;
-      const res = await fetch(urlString, {
-        headers: {
-          "x-nxopen-api-key": API_KEY,
-        },
-      });
+  const userId = body.userId;
 
-      if (!res.ok) {
-        console.error("API 응답 실패:", res.status);
-        return NextResponse.json(
-          { message: "Nexon API 오류" },
-          { status: res.status }
-        );
-      }
+  const db = (await connectDB).db("donson");
+  const user = await db.collection("user").findOne({ userId });
 
-      const data = await res.json();
-      console.log("불러온 API 값", data.ocid);
-
-      await db
-        .collection("user")
-        .updateOne({ userId: userId }, { $set: { oicd: data.ocid } });
-
-      return NextResponse.json(data.ocid);
-    }
-  } catch (error) {
-    console.error("서버 에러입니다.", error);
-    return NextResponse.json({ message: "서버 내부 오류" }, { status: 500 });
+  if (!user) {
+    return NextResponse.json({ message: "사용자 없음" }, { status: 404 });
   }
+
+  console.log("유저 정보:", user);
+
+  const ocid = user.ocid;
+  return NextResponse.json({ ocid });
 }
