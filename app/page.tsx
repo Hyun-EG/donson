635dec5776fc8ede00188951bac53583b312a999
Hyun-EG <codiee@naver.com>
2025-06-02 19:40:59 +0900

    Feat: 인포박스 프롭스 추가 및 로그인한 쿠키에서 userId 게또

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
-import { verifyJWT } from "@/util/jwt";
+import { MyJwtPayload, verifyJWT } from "@/util/jwt";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import InfoBox from "./_components/InfoBox";
 
 const page = async () => {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
-  const decoded = token ? verifyJWT(token) : null;
+  const decoded = token ? (verifyJWT(token) as MyJwtPayload) : null;
+  const userId = decoded?.userId;
+
+  try {
+  } catch (error) {}
 
   if (!decoded) {
     redirect("/signin");
   }
-  return <section>Main page</section>;
+  return (
+    <section>
+      Main page
+      <InfoBox userId={userId!} />
+    </section>
+  );
 };
 
 export default page;
