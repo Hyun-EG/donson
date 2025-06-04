bde7ff18a017d0bab98a6706920d36063ac872e6
Hyun-EG <codiee@naver.com>
2025-06-04 20:40:31 +0900

    Types: 하이퍼 스탯 타입 추가

--- app/info/_components/types.ts
+++ app/info/_components/types.ts
@@ -9,3 +9,26 @@ export type CharStat = {
   final_stat: FinalStat[];
   remain_ap: number;
 };
+
+export interface HyperStatDetail {
+  stat_type: string;
+  stat_point: number;
+  stat_level: number;
+  stat_increase: string;
+}
+
+export interface HyperStat {
+  date: string;
+  character_class: string;
+  use_preset_no: string;
+  use_available_hyper_stat: number;
+
+  hyper_stat_preset_1: HyperStatDetail[];
+  hyper_stat_preset_1_remain_point: number;
+
+  hyper_stat_preset_2: HyperStatDetail[];
+  hyper_stat_preset_2_remain_point: number;
+
+  hyper_stat_preset_3: HyperStatDetail[];
+  hyper_stat_preset_3_remain_point: number;
+}
