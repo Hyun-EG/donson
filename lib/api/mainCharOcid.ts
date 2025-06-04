a1fe0926f30d41af86b1b9e4fad2be45c954bdaa
Hyun-EG <codiee@naver.com>
2025-06-04 17:29:25 +0900

    Fix: OCID 관련 API 함수명 조금더 직관적으로 변경

---
+++ lib/api/mainCharOcid.ts
@@ -0,0 +1,30 @@
+export const addOcid = async (userId: string) => {
+  const res = await fetch("/api/add-char-ocid", {
+    method: "POST",
+    body: JSON.stringify({ userId }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`OCID 저장 실패: ${res.status}`);
+  }
+};
+
+export const getCharOcid = async (userId: string): Promise<string> => {
+  const res = await fetch("/api/get-char-ocid", {
+    method: "POST",
+    body: JSON.stringify({ userId }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`OCID 불러오기 실패: ${res.status}`);
+  }
+
+  const data = await res.json();
+  return data.ocid;
+};
