7a21e34b2905512865fba965a64c909c35399542
Hyun-EG <codiee@naver.com>
2025-06-04 17:26:49 +0900

    Env: 넥스트 외부 도메인 허용 (넥슨 메이플 캐릭터 이미지)

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,21 @@
 import type { NextConfig } from "next";
 
-const withPWA = require('next-pwa')({
-  dest:"public",
-  register:true,
-  skipWaiting:true,
-})
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "open.api.nexon.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
-module.exports = withPWA(nextConfig)
-
+module.exports = withPWA(nextConfig);
