9d29a7e099db20b1b8850db520e91431fc63d25a
Hyun-EG <codiee@naver.com>
2025-06-04 19:28:40 +0900

    Add: 종합능력치컴포넌트 추가

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,14 +1,21 @@
 import { getUserCookies } from "@/util/getUserCookie";
 import { redirect } from "next/navigation";
 import React from "react";
+import TotalStatBox from "./_components/TotalStatBox";
 
 const Info = async () => {
   const cookie = await getUserCookies();
-
   if (!cookie) {
     redirect("/signin");
   }
-  return <section>Info page</section>;
+
+  return (
+    <section className="w-full flex flex-col ">
+      <article className="flex flex-col">
+        <TotalStatBox />
+      </article>
+    </section>
+  );
 };
 
 export default Info;
