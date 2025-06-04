643b280d50dff0e97f4e7dc72a52c2cc16a7d6f6
Hyun-EG <codiee@naver.com>
2025-06-04 17:27:51 +0900

    Types: 넥슨 api 캐릭터 기본정보 타입 추가

---
+++ app/_components/types.ts
@@ -0,0 +1,10 @@
+type CharInfo = {
+  character_image?: string;
+  character_name?: string;
+  character_level?: number;
+  character_guild_name?: string;
+  world_name?: string;
+  character_date_create?: string;
+  character_exp_rate?: string;
+  liberation_quest_clear_flag: string;
+};
