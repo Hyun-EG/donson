530bf1e062475d05bf033a204a1032c454814912
Hyun-EG <codiee@naver.com>
2025-06-04 19:56:13 +0900

    Add: 하이퍼스탯 박스 기본 레이아웃 추가

---
+++ app/info/_components/HyperStatBox.tsx
@@ -0,0 +1,12 @@
+const HyperStatBox = () => {
+  return (
+    <section className="px-3 py-2 flex flex-col border-b border-black">
+      <article className="flex justify-between items-center">
+        <h1 className="text-xl font-bold">하이퍼 스탯</h1>
+        <span>▽</span>
+      </article>
+    </section>
+  );
+};
+
+export default HyperStatBox;
