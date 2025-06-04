b8e0c9d7ca7038900daa4eeb2bbebf3223cf1b36
Hyun-EG <codiee@naver.com>
2025-06-04 19:27:16 +0900

    Fix: 바디값 로직변경 (구조분해) , 각 api 에러 상태코드 반환값 추가

--- app/api/get-char-ocid/route.ts
+++ app/api/get-char-ocid/route.ts
@@ -2,18 +2,32 @@ import { connectDB } from "@/util/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const userId = body.userId;
+  try {
+    const { userId } = await req.json();
+    if (!userId) {
+      return NextResponse.json({ message: "userId 없음" }, { status: 400 });
+    }
 
-  const db = (await connectDB).db("donson");
-  const user = await db.collection("user").findOne({ userId });
+    const db = (await connectDB).db("donson");
+    const user = await db.collection("user").findOne({ userId });
 
-  if (!user) {
-    return NextResponse.json({ message: "사용자 없음" }, { status: 404 });
+    if (!user) {
+      return NextResponse.json({ message: "사용자 없음" }, { status: 404 });
+    }
+
+    console.log("유저 정보:", user);
+
+    if (!user.ocid) {
+      return NextResponse.json(
+        { message: "OCID가 등록되지 않았습니다." },
+        { status: 400 }
+      );
+    }
+    const ocid = user.ocid;
+
+    return NextResponse.json({ ocid });
+  } catch (error) {
+    console.error("API 오류", error);
+    return NextResponse.json({ message: "서버 에러 발생" }, { status: 500 });
   }
-
-  console.log("유저 정보:", user);
-
-  const ocid = user.ocid;
-  return NextResponse.json({ ocid });
 }
