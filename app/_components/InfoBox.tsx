b7efe0be4ad36c455a122eeeeec7150a65afebae
Hyun-EG <codiee@naver.com>
2025-06-04 17:30:53 +0900

    Feat: 캐릭터 정보 박스 디자인 및 기본정보 추가 api 연결

--- app/_components/InfoBox.tsx
+++ app/_components/InfoBox.tsx
@@ -1,50 +1,113 @@
 "use client";
 import { useEffect, useState } from "react";
+import { addOcid, getCharOcid } from "@/lib/api/mainCharOcid";
+import { charBasicInfo } from "@/lib/api/charBasicInfo";
+import Image from "next/image";
 
 const InfoBox = ({ userId }: { userId: string }) => {
   const [ocid, setOcid] = useState("");
-  useEffect(() => {
-    const getOcid = async () => {
-      try {
-        const res = await fetch("/api/add-char-ocid", {
-          method: "POST",
-          body: JSON.stringify({ userId }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+  const [charInfo, setCharInfo] = useState<CharInfo | null>(null);
 
-        if (!res.ok) {
-          console.error("OCID 저장 실패", res.status);
-        }
+  useEffect(() => {
+    const saveOcid = async () => {
+      try {
+        await addOcid(userId);
       } catch (err) {
-        console.error("서버 요청 에러", err);
+        console.error(err);
       }
     };
-
-    getOcid();
+    saveOcid();
   }, [userId]);
 
   useEffect(() => {
-    const getCharInfo = async () => {
-      const res = await fetch("/api/get-char-ocid", {
-        method: "POST",
-        body: JSON.stringify({ userId }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await res.json();
-      console.log(data);
-      setOcid(data.ocid);
+    const fetchOcid = async () => {
+      try {
+        const ocid = await getCharOcid(userId);
+        setOcid(ocid);
+      } catch (err) {
+        console.error(err);
+      }
     };
-    getCharInfo();
-  });
+    fetchOcid();
+  }, [userId]);
+
+  useEffect(() => {
+    if (!ocid) return;
+
+    const getCharBasicInfo = async () => {
+      try {
+        const res = await charBasicInfo({ ocid });
+        console.log(res);
+        setCharInfo(res);
+      } catch (error) {
+        console.error("캐릭터 정보를를 불러오지 못했습니다.", error);
+      }
+    };
+    getCharBasicInfo();
+  }, [ocid]);
+
+  useEffect(() => {
+    if (charInfo) console.log("최신 캐릭터 정보:", charInfo);
+    if (ocid) console.log("ocid:", ocid);
+  }, [charInfo, ocid]);
+
   return (
-    <div>
-      <div>{userId}</div>
-      <div>{ocid}</div>
-    </div>
+    <main className="flex flex-col items-center">
+      <section className="flex">
+        {charInfo?.character_image && (
+          <Image
+            className="border rounded-xl"
+            src={charInfo?.character_image}
+            alt="캐릭터 이미지"
+            width={120}
+            height={120}
+          />
+        )}
+        <article className="flex flex-col px-4">
+          <p>
+            월드: <span className="font-bold">{charInfo?.world_name}</span>
+          </p>
+          <p>
+            용사: <span className="font-bold">{charInfo?.character_name}</span>
+            님
+          </p>
+          <p>
+            해방 여부:
+            <span>
+              {charInfo?.liberation_quest_clear_flag === "true"
+                ? "해방 완료"
+                : "해방도 못했네"}
+            </span>
+          </p>
+          <p>
+            레벨: <span className="font-bold">{charInfo?.character_level}</span>
+          </p>
+          <p>
+            길드:{" "}
+            <span className="font-bold">{charInfo?.character_guild_name}</span>
+          </p>
+          <p>
+            생성 날짜:
+            <span className="font-bold">
+              {charInfo?.character_date_create?.split("T")[0]}
+            </span>
+          </p>
+        </article>
+      </section>
+      <aside className="w-full flex flex-col justify-center">
+        <h1 className="text-sm font-bold">Exp</h1>
+        <div className="w-full flex items-center gap-2">
+          <div className="w-[85%] h-4 py-1">
+            <div
+              className={`w-[${charInfo?.character_exp_rate}%] h-2 bg-green-300 rounded-r-xl`}
+            />
+          </div>
+          <div className="w-[15%]">
+            <p className="text-sm">{charInfo?.character_exp_rate}%</p>
+          </div>
+        </div>
+      </aside>
+    </main>
   );
 };
 
