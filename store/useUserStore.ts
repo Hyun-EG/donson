26e7724b01abeba9b0d47169377e600e330da815
Hyun-EG <codiee@naver.com>
2025-06-09 16:53:48 +0900

    Feat: 유저 정보 (ID, OCID) 로컬스토리지 저장

--- store/useUserStore.ts
+++ store/useUserStore.ts
@@ -1,5 +1,7 @@
-import { addOcid, getCharOcid } from "@/lib/api/mainCharOcid";
+// store/useUserStore.ts
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { addOcid, getCharOcid } from "@/lib/api/mainCharOcid";
 
 type UserStore = {
   userId: string;
@@ -7,19 +9,25 @@ type UserStore = {
   setUserIdAndOcid: (userId: string) => Promise<void>;
 };
 
-const useUserStore = create<UserStore>((set) => ({
-  userId: "",
-  ocid: "",
-
-  setUserIdAndOcid: async (userId: string) => {
-    try {
-      await addOcid(userId);
-      const ocid = await getCharOcid(userId);
-      set({ userId, ocid });
-    } catch (error) {
-      console.error("유저 ID 및 OCID 설정 실패", error);
+const useUserStore = create(
+  persist<UserStore>(
+    (set) => ({
+      userId: "",
+      ocid: "",
+      setUserIdAndOcid: async (userId: string) => {
+        try {
+          await addOcid(userId);
+          const ocid = await getCharOcid(userId);
+          set({ userId, ocid });
+        } catch (err) {
+          console.error("유저 ID 및 OCID 설정 실패", err);
+        }
+      },
+    }),
+    {
+      name: "user-store", 
     }
-  },
-}));
+  )
+);
 
 export default useUserStore;
