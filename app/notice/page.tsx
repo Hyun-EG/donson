babb5d6a4654ed2771e4ed8452a48fdf1cdaecae
Hyun-EG <codiee@naver.com>
2025-06-10 20:27:41 +0900

    Add: 공지사항 클라이언트 컴포넌트 추가

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -1,6 +1,7 @@
 import { getUserCookies } from "@/util/getUserCookie";
 import { redirect } from "next/navigation";
 import React from "react";
+import NoticeBox from "./_components/NoticeBox";
 
 const Notice = async () => {
   const cookie = await getUserCookies();
@@ -8,7 +9,12 @@ const Notice = async () => {
   if (!cookie) {
     redirect("/signin");
   }
-  return <main className="flex flex-col gap-2">공지사항</main>;
+  return (
+    <main className="flex flex-col gap-2">
+      <p className="text-center font-bold text-lg">공지사항</p>
+      <NoticeBox />
+    </main>
+  );
 };
 
 export default Notice;
