e9272d3be436d021b2f97c05481f38a038183f21
Hyun-EG <codiee@naver.com>
2025-06-10 21:07:35 +0900

    Feat: 어빌리티 API 추가

---
+++ app/api/get-char-ability/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { ocid } = await req.json();
+  try {
+    if (!ocid) {
+      return NextResponse.json(
+        { message: "ocid가 없습니다.", status: 400 },
+        { status: 400 }
+      );
+    }
+    const res = await fetch(
+      `https://open.api.nexon.com/maplestory/v1/character/ability?ocid=${ocid}`
+    );
+    if (!res.ok) {
+      return NextResponse.json(
+        { message: "응답값이 존재하지 않습니다.", status: 400 },
+        { status: 400 }
+      );
+    }
+
+    const data = await res.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("캐릭터 어빌리티 능력치를 얻어오지 못했습니다.", error);
+    return NextResponse.json(
+      { message: "서버 에러", status: 500 },
+      { status: 500 }
+    );
+  }
+}
