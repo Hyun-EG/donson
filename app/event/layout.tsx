f8e3ef9cf95d036202c2f73081e39c6b8609870c
Hyun-EG <codiee@naver.com>
2025-06-10 18:07:53 +0900

    Fix: 폴더 변경

---
+++ app/event/layout.tsx
@@ -0,0 +1,7 @@
+export default function NoticeLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
--- app/event/page.tsx
+++ app/event/page.tsx
@@ -1,5 +1,20 @@
-const Event = () => {
-  return <div>event page</div>;
+import { getUserCookies } from "@/util/getUserCookie";
+import { redirect } from "next/navigation";
+import React from "react";
+import NoticeEventBox from "./_components/NoticeEventBox";
+
+const Notice = async () => {
+  const cookie = await getUserCookies();
+
+  if (!cookie) {
+    redirect("/signin");
+  }
+  return (
+    <main className="flex flex-col gap-2">
+      <h6 className="text-center text-lg font-bold">이벤트</h6>
+      <NoticeEventBox />
+    </main>
+  );
 };
 
-export default Event;
+export default Notice;
