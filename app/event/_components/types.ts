0dec91837a16bb5f5537f1786bc2f59f8ff44f22
Hyun-EG <codiee@naver.com>
2025-06-10 18:08:40 +0900

    Feat: 이벤트 타입 선언

---
+++ app/event/_components/types.ts
@@ -0,0 +1,12 @@
+type NoticeListType = {
+  event_notice?: [
+    {
+      title: string;
+      url: string;
+      notice_id: number;
+      date: string;
+      date_event_start: string;
+      date_event_end: string;
+    }
+  ];
+};
