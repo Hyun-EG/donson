d617703db5572defa7b022daa566934e00809770
seonghyun <codiee@naver.com>
2025-06-11 21:29:12 +0900

    Fix: 하이퍼 스탯 api 로직 추가

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -13,7 +13,8 @@ const Info = async () => {
 
   const { ocid } = cookie;
 
-  const res = await fetch(
+  // 종합스탯
+  const resTotalStat = await fetch(
     `https://open.api.nexon.com/maplestory/v1/character/stat?ocid=${ocid}`,
     {
       headers: {
@@ -22,18 +23,35 @@ const Info = async () => {
     }
   );
 
-  if (!res.ok) {
+  if (!resTotalStat.ok) {
     console.log("캐릭터 종합 능력치 정보를 받지 못했습니다.");
     return;
   }
 
-  const totalStat = await res.json();
+  const totalStat = await resTotalStat.json();
+
+  // 하이퍼스탯
+  const resHyperStat = await fetch(
+    `https://open.api.nexon.com/maplestory/v1/character/hyper-stat?ocid=${ocid}`,
+    {
+      headers: {
+        "x-nxopen-api-key": process.env.NEXON_API_KEY!,
+      },
+    }
+  );
+
+  if (!resHyperStat.ok) {
+    console.log("캐릭터 종합 능력치 정보를 받지 못했습니다.");
+    return;
+  }
+
+  const totalHyperStat = await resHyperStat.json();
 
   return (
     <section className="w-full flex flex-col ">
       <article className="flex flex-col">
         <TotalStatBox totalStat={totalStat} />
-        <HyperStatBox />
+        <HyperStatBox totalHyperStat={totalHyperStat} />
         <AbilityBox />
       </article>
     </section>
