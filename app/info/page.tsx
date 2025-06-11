7d5abbcecb0ef052192db279d0a78ba2dd90db9a
seonghyun <codiee@naver.com>
2025-06-11 21:18:59 +0900

    Fix: 기존 앱라우트 api 삭제 및 서버에서 즉시 페칭

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -11,10 +11,28 @@ const Info = async () => {
     redirect("/signin");
   }
 
+  const { ocid } = cookie;
+
+  const res = await fetch(
+    `https://open.api.nexon.com/maplestory/v1/character/stat?ocid=${ocid}`,
+    {
+      headers: {
+        "x-nxopen-api-key": process.env.NEXON_API_KEY!,
+      },
+    }
+  );
+
+  if (!res.ok) {
+    console.log("캐릭터 종합 능력치 정보를 받지 못했습니다.");
+    return;
+  }
+
+  const totalStat = await res.json();
+
   return (
     <section className="w-full flex flex-col ">
       <article className="flex flex-col">
-        <TotalStatBox />
+        <TotalStatBox totalStat={totalStat} />
         <HyperStatBox />
         <AbilityBox />
       </article>
