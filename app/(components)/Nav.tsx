a5d9aaaf94f85757e769124c1ce78adb34a68211
seonghyun <codiee@naver.com>
2025-06-13 19:35:19 +0900

    Feat: 서브메뉴 추가 , 메뉴 아이콘 추가, 로그아웃 api 연결

--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import logo from "@/public/icons/icon-logo.png";
+import menu from "@/public/icons/icon-menu.svg";
 import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
+import SubNav from "./SubNav";
 
 const Nav = () => {
   const router = useRouter();
   const pathName = usePathname();
+  const [isShowMenu, setIsShowMenu] = useState(false);
   if (pathName === "/signin" || pathName === "/signup") {
     return null;
   }
   return (
-    <nav className="fixed top-0 left-0 w-full h-14 px-2 flex items-center border-b border-[#bebebe] bg-white">
+    <nav className="fixed top-0 left-0 w-full h-14 px-3 flex justify-between items-center border-b border-[#bebebe] bg-white">
       <ul className="flex items-center gap-4 text-sm">
         <Image
           onClick={() => {
@@ -34,6 +37,14 @@ const Nav = () => {
           <Link href="/event">이벤트</Link>
         </li>
       </ul>
+      <button
+        onClick={() => {
+          setIsShowMenu(true);
+        }}
+      >
+        <Image src={menu} alt="메뉴 아이콘" width={20} priority />
+      </button>
+      {isShowMenu && <SubNav setIsShowMenu={setIsShowMenu} />}
     </nav>
   );
 };
