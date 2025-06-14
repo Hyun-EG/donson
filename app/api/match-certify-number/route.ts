3056a9397e4210400eb1e3a097337b89c51f049c
seonghyun <codiee@naver.com>
2025-06-15 00:22:45 +0900

    Feat: 인증번호 일치 확인 api 추가

---
+++ app/api/match-certify-number/route.ts
@@ -0,0 +1,21 @@
+import { connectDB } from "@/util/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { userEmail, certifyNo } = await req.json();
+  const db = (await connectDB).db("donson");
+
+  const match = await db.collection("certify").findOne({
+    userEmail,
+    certifyNo: certifyNo,
+  });
+
+  if (!match) {
+    return NextResponse.json(
+      { message: "인증 실패", valid: false },
+      { status: 400 }
+    );
+  }
+
+  return NextResponse.json({ message: "인증 성공", valid: true });
+}
