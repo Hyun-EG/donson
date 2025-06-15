36f0015e8bc58b2d88a7c808bc3c0ec97eaea504
seonghyun <codiee@naver.com>
2025-06-15 21:56:50 +0900

    Feat: 로그인 작업 로딩 오버레이 추가

--- app/signin/_components/LoginForm.tsx
+++ app/signin/_components/LoginForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LoadingOverlay from "@/app/(components)/LoadingOverlay";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
@@ -7,59 +8,70 @@ const LoginForm = () => {
   const router = useRouter();
   const [userId, setIUserId] = useState("");
   const [userPassword, setUserPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleLogin = async () => {
-    const res = await fetch("/api/signin", {
-      method: "POST",
-      body: JSON.stringify({ userId, userPassword }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    setIsLoading(true);
+    try {
+      const res = await fetch("/api/signin", {
+        method: "POST",
+        body: JSON.stringify({ userId, userPassword }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    const result = await res.json();
+      const result = await res.json();
 
-    if (res.ok) {
-      router.push("/");
-    } else {
-      alert(result.message);
+      if (res.ok) {
+        router.push("/");
+      } else {
+        alert(result.message);
+      }
+    } catch (error) {
+      console.error("로그인 중 에러가 발생하였습니다.", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <form className="flex flex-col gap-2">
-      <input
-        value={userId}
-        onChange={(e) => {
-          setIUserId(e.target.value);
-        }}
-        onKeyDown={(e) => {
-          e.key === "Enter" ? handleLogin() : null;
-        }}
-        className="w-full h-10 px-2 border border-[#bebebe] bg-white focus:ring-2 focus:ring-sky-500 focus:outline-none"
-        type="text"
-        placeholder="아이디를 입력해주세요."
-      />
-      <input
-        value={userPassword}
-        onChange={(e) => {
-          setUserPassword(e.target.value);
-        }}
-        onKeyDown={(e) => {
-          e.key === "Enter" ? handleLogin() : null;
-        }}
-        className="w-full h-10 px-2 border border-[#bebebe] bg-white focus:ring-2 focus:ring-sky-500 focus:outline-none"
-        type="password"
-        placeholder="비밀번호를 입력해주세요."
-      />
-      <button
-        className="w-full h-10 bg-sky-500 text-white rounded-xl"
-        type="button"
-        onClick={handleLogin}
-      >
-        로그인
-      </button>
-    </form>
+    <>
+      {isLoading && <LoadingOverlay />}
+      <form className="flex flex-col gap-2">
+        <input
+          value={userId}
+          onChange={(e) => {
+            setIUserId(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            e.key === "Enter" ? handleLogin() : null;
+          }}
+          className="w-full h-10 px-2 border border-[#bebebe] bg-white focus:ring-2 focus:ring-sky-500 focus:outline-none"
+          type="text"
+          placeholder="아이디를 입력해주세요."
+        />
+        <input
+          value={userPassword}
+          onChange={(e) => {
+            setUserPassword(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            e.key === "Enter" ? handleLogin() : null;
+          }}
+          className="w-full h-10 px-2 border border-[#bebebe] bg-white focus:ring-2 focus:ring-sky-500 focus:outline-none"
+          type="password"
+          placeholder="비밀번호를 입력해주세요."
+        />
+        <button
+          className="w-full h-10 bg-sky-500 text-white rounded-xl"
+          type="button"
+          onClick={handleLogin}
+        >
+          로그인
+        </button>
+      </form>
+    </>
   );
 };
 
