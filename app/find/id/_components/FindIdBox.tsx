469bade28e10fabbeb5023572012bd2cacbdab06
seonghyun <codiee@naver.com>
2025-06-15 16:21:26 +0900

    Feat: 아이디 찾기 로딩 오버레이 추가

--- app/find/id/_components/FindIdBox.tsx
+++ app/find/id/_components/FindIdBox.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Modal from "./Modal";
+import LoadingOverlay from "@/app/(components)/LoadingOverlay";
 
 const FindIdBox = () => {
   const [nameInputValue, setNameInputValue] = useState("");
@@ -9,7 +10,12 @@ const FindIdBox = () => {
   const [foundUserId, setFoundUserId] = useState(null);
   const [isShowModal, setIsShowModal] = useState(false);
 
-  const handleFindId = async () => {
+  const [isShowLoading, setIsShowLoading] = useState(false);
+
+  const handleFindId = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsShowLoading(true);
+
     try {
       if (!nameInputValue || !emailInputValue) {
         alert("모든 입력 필드를 채워주세요.");
@@ -30,6 +36,8 @@ const FindIdBox = () => {
       setFoundUserId(data);
     } catch (error) {
       console.error("서버 에러", error);
+    } finally {
+      setIsShowLoading(false);
     }
   };
 
@@ -40,40 +48,42 @@ const FindIdBox = () => {
   }, [foundUserId]);
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleFindId();
-        setNameInputValue("");
-        setEmailInputValue("");
-      }}
-      className="w-full flex flex-col gap-2"
-    >
-      <input
-        type="text"
-        className="w-full h-10 px-2 border border-[#bebebe] bg-white focus:ring-2 focus:ring-sky-500 focus:outline-none"
-        placeholder="성함을 입력해주세요."
-        value={nameInputValue}
-        onChange={(e) => {
-          setNameInputValue(e.target.value);
+    <div>
+      {isShowLoading && <LoadingOverlay />}
+      <form
+        onSubmit={(e) => {
+          handleFindId(e);
+          setNameInputValue("");
+          setEmailInputValue("");
         }}
-      />
-      <input
-        type="text"
-        className="w-full h-10 px-2 border border-[#bebebe] bg-white focus:ring-2 focus:ring-sky-500 focus:outline-none"
-        placeholder="이메일을 입력해주세요."
-        value={emailInputValue}
-        onChange={(e) => {
-          setEmailInputValue(e.target.value);
-        }}
-      />
-      <button className="w-full h-10 bg-sky-500 text-white rounded-xl">
-        아이디 찾기
-      </button>
-      {isShowModal && (
-        <Modal setIsShowModal={setIsShowModal} foundUserId={foundUserId} />
-      )}
-    </form>
+        className="w-full flex flex-col gap-2"
+      >
+        <input
+          type="text"
+          className="w-full h-10 px-2 border border-[#bebebe] bg-white focus:ring-2 focus:ring-sky-500 focus:outline-none"
+          placeholder="성함을 입력해주세요."
+          value={nameInputValue}
+          onChange={(e) => {
+            setNameInputValue(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          className="w-full h-10 px-2 border border-[#bebebe] bg-white focus:ring-2 focus:ring-sky-500 focus:outline-none"
+          placeholder="이메일을 입력해주세요."
+          value={emailInputValue}
+          onChange={(e) => {
+            setEmailInputValue(e.target.value);
+          }}
+        />
+        <button className="w-full h-10 bg-sky-500 text-white rounded-xl">
+          아이디 찾기
+        </button>
+        {isShowModal && (
+          <Modal setIsShowModal={setIsShowModal} foundUserId={foundUserId} />
+        )}
+      </form>
+    </div>
   );
 };
 
