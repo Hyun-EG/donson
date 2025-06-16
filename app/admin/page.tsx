31090741aa5f6ce617bfb79e64db31f9af4b966c
seonghyun <codiee@naver.com>
2025-06-17 01:38:01 +0900

    Feat: 관리자 페이지 문의내역 db 데이터 프롭스 인계

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,23 @@
-const Admin = () => {
-  return <div>관리자페이지입니다.</div>;
+import { connectDB } from "@/util/mongodb";
+import InquiryDetailsBox from "./_components/InquiryDetailsBox";
+import { Contact } from "./_components/types";
+
+const Admin = async () => {
+  const db = (await connectDB).db("donson");
+  const rawContact = await db.collection("contact").find().toArray();
+
+  const contact: Contact[] = rawContact.map((doc) => ({
+    userId: doc.userId,
+    category: doc.category,
+    content: doc.content,
+  }));
+
+  return (
+    <section>
+      <h1 className="text-lg text-center font-bold">관리자 메뉴</h1>
+      <InquiryDetailsBox contacts={contact} />
+    </section>
+  );
 };
 
 export default Admin;
