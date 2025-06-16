633e67aca6b639031030a39a36084082187485aa
Hyun-EG <codiee@naver.com>
2025-06-16 19:06:17 +0900

    Feat: 유저아이디 데이터 인계

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,21 @@
 import { getUserCookies } from "@/util/getUserCookie";
 import { redirect } from "next/navigation";
 import React from "react";
+import ContactBox from "./_components/ContactBox";
 
 const Contact = async () => {
   const cookie = await getUserCookies();
   if (!cookie) {
     redirect("/signin");
   }
-  return <p>미개발구역</p>;
+  const userId = cookie.userId;
+
+  return (
+    <section>
+      <h1 className="text-center text-lg font-bold">고객문의</h1>
+      <ContactBox userId={userId} />
+    </section>
+  );
 };
 
 export default Contact;
