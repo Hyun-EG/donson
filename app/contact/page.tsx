26560692d697b237b9f9d988ca007375b163f77f
Hyun-EG <codiee@naver.com>
2025-06-16 17:55:18 +0900

    Feat: 토큰 없으면 로그인으로 이동 로직 추가

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,12 @@
+import { getUserCookies } from "@/util/getUserCookie";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Contact = () => {
+const Contact = async () => {
+  const cookie = await getUserCookies();
+  if (!cookie) {
+    redirect("/signin");
+  }
   return <p>미개발구역</p>;
 };
 
--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import MyPageBox from "./_components/MyPageBox";
+import { getUserCookies } from "@/util/getUserCookie";
+import { redirect } from "next/navigation";
 
-const MyPage = () => {
+const MyPage = async () => {
+  const cookie = await getUserCookies();
+  if (!cookie) {
+    redirect("/signin");
+  }
   return (
     <section>
-      <h1 className="font-bold text-center text-lg">마이페이지</h1>
+      <h1 className="mb-4 font-bold text-center text-lg">마이페이지</h1>
       <main>
         <MyPageBox />
       </main>
