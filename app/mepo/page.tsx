5b93f7949b13c6cab8c5398dc68ff9fd53e527e7
Hyun-EG <codiee@naver.com>
2025-06-17 20:39:10 +0900

    Feat: 메포 시세 db 데이터 불러오고 props 인계

--- app/mepo/page.tsx
+++ app/mepo/page.tsx
@@ -1,13 +1,42 @@
 import { getUserCookies } from "@/util/getUserCookie";
+import { connectDB } from "@/util/mongodb";
 import { redirect } from "next/navigation";
+import MepoGraph from "./_components/MepoGraph";
 import React from "react";
+import { MepoPirce } from "./_components/types";
+import MepoStatBox from "./_components/MepoStatBox";
 
 const Mepo = async () => {
   const cookie = await getUserCookies();
   if (!cookie) {
     redirect("/signin");
   }
-  return <p>미개발구역</p>;
+
+  const db = (await connectDB).db("donson");
+  const mepoPrice: MepoPirce[] = (
+    await db.collection("maple-point").find().sort({ updateAt: 1 }).toArray()
+  ).map((item) => ({
+    ...item,
+    _id: item._id.toString(),
+    price: item.price,
+    updateAt: item.updateAt,
+  }));
+
+  console.log(mepoPrice);
+
+  return (
+    <section className="w-full h-full sm500:h-96 flex flex-col items-center">
+      <h1 className="font-bold text-lg">메포 시세</h1>
+      <aside className="mt-2 text-xs">
+        <p>메포 시세는 넥슨 Open API 미제공으로 매일 수동 등록됩니다.</p>
+        <p>등록시작일시 : 2025-06-17</p>
+      </aside>
+      <main className="w-full h-52 mt-4 z-[-1]">
+        <MepoGraph data={mepoPrice} />
+      </main>
+      <MepoStatBox data={mepoPrice} />
+    </section>
+  );
 };
 
 export default Mepo;
