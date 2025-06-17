4d29593fa31829c8302ffa59f9edbf2089eb6389
seonghyun <codiee@naver.com>
2025-06-18 03:12:24 +0900

    Add: 렌 페이지 추가 및 렌박스 연결

---
+++ app/ren/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RenBox from "./_components/RenBox";
+
+const Ren = () => {
+  return (
+    <section>
+      <h1 className="text-center font-bold">렌 육성</h1>
+      <RenBox />
+    </section>
+  );
+};
+
+export default Ren;
