2d0c42630f5dda638ae46ee1905ca67117759bba
Hyun-EG <codiee@naver.com>
2025-06-17 18:16:58 +0900

    Feat: 페이지네이션 전체페이지, 문의데이터, 페이지당 보여줄 컨텐츠 상태값 인계 및 연결

--- app/admin/_components/InquiryDetailsBox.tsx
+++ app/admin/_components/InquiryDetailsBox.tsx
@@ -2,12 +2,18 @@
 import React, { useState } from "react";
 import { Contact } from "./types";
 import LoadingOverlay from "@/app/(components)/LoadingOverlay";
+import Pagination from "./Pagination";
 
 const InquiryDetailsBox = ({ contacts }: { contacts: Contact[] }) => {
   const [selectedIdx, setSelectedIdx] = useState<null | string>(null);
   const [localContacts, setLocalContacts] = useState<Contact[]>(contacts);
   const [deletingId, setDeletingId] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [displayedContacts, setDisplayedContacts] = useState<Contact[]>(
+    contacts.slice(0, 3)
+  );
+
+  const totalPage = Math.ceil(contacts.length / 3);
 
   const handleRemoveContact = async (
     e: React.MouseEvent<HTMLButtonElement>,
@@ -50,41 +56,53 @@ const InquiryDetailsBox = ({ contacts }: { contacts: Contact[] }) => {
   };
 
   return (
-    <section className="mt-4">
-      {isLoading && <LoadingOverlay />}
-      {localContacts.map((contact, index) => (
-        <article key={index} className="p-2 border-b">
-          <div
-            onClick={() => {
-              setSelectedIdx((prev) => (prev ? null : contact._id));
-            }}
-          >
-            <header className="flex justify-between">
-              <p>카테고리: {contact.category}</p>
-              <button
-                onClick={(e) => {
-                  handleRemoveContact(e, contact._id);
-                }}
-                className="w-14 h-6 px-2 py-1 flex justify-center items-center border rounded-lg text-xs"
-                disabled={deletingId === contact._id}
-              >
-                {deletingId === contact._id ? "삭제 중" : "삭제"}
-              </button>
-            </header>
-            <div className="pr-5 flex justify-between items-center">
-              <p>유저 ID: {contact.userId}</p>
-              <span className="text-xs">
-                {selectedIdx === contact._id ? "△" : "▽"}
-              </span>
+    <section className="w-full h-80 mt-4 flex flex-col justify-between border-b">
+      <div>
+        <h1 className="font-bold">용사님들의 문의내역</h1>
+        {isLoading && <LoadingOverlay />}
+        {displayedContacts.map((contact, index) => (
+          <article key={index} className="flex flex-col p-2 border-b">
+            <div
+              onClick={() => {
+                setSelectedIdx((prev) => (prev ? null : contact._id));
+              }}
+            >
+              <header className="flex justify-between">
+                <p>카테고리: {contact.category}</p>
+                <button
+                  onClick={(e) => {
+                    handleRemoveContact(e, contact._id);
+                  }}
+                  className="w-14 h-6 px-2 py-1 flex justify-center items-center border rounded-lg text-xs"
+                  disabled={deletingId === contact._id}
+                >
+                  {deletingId === contact._id ? "삭제 중" : "삭제"}
+                </button>
+              </header>
+              <div className="pr-5 flex justify-between items-center">
+                <p>유저 ID: {contact.userId}</p>
+                <span className="text-xs">
+                  {selectedIdx === contact._id ? "△" : "▽"}
+                </span>
+              </div>
             </div>
-          </div>
-          {selectedIdx === contact._id && (
-            <main className="px-1 py-2 bg-[#bebebe] rounded-lg">
-              <p>내용: {contact.content}</p>
-            </main>
-          )}
-        </article>
-      ))}
+            {selectedIdx === contact._id && (
+              <main className="px-1 py-2 bg-[#bebebe] rounded-lg">
+                <p>내용: {contact.content}</p>
+              </main>
+            )}
+          </article>
+        ))}
+      </div>
+      <footer className="w-full h-10 flex justify-center items-center gap-2">
+        <Pagination
+          curTotalPage={totalPage}
+          allContacts={localContacts}
+          onPageChange={(start, end) => {
+            setDisplayedContacts(localContacts.slice(start, end));
+          }}
+        />
+      </footer>
     </section>
   );
 };
