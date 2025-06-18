8f45b00c64b704f0f8e0a0e6176dd80a019fd56a
Hyun-EG <codiee@naver.com>
2025-06-18 15:00:32 +0900

    Docs: 설명 문구 추가

--- app/ren/page.tsx
+++ app/ren/page.tsx
@@ -4,7 +4,12 @@ import RenBox from "./_components/RenBox";
 const Ren = () => {
   return (
     <section>
-      <h1 className="text-center font-bold">렌 육성</h1>
+      <h1 className="text-center font-bold">렌 사냥터|빌드</h1>
+      <article className="my-4">
+        <p className="text-sm text-red-500 text-center">
+          고효율 추천 사냥터 정리
+        </p>
+      </article>
       <RenBox />
     </section>
   );
