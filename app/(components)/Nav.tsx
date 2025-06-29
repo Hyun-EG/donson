8ecec4bf1481a0b18f131fa2978354f4906912c5
Hyun-EG <codiee@naver.com>
2025-06-29 18:03:09 +0900

    Refactor: 기존 클라이언트 컴포넌트에서 서버컴포넌트로 교체

--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -1,109 +1,30 @@
-"use client";
+import { connectDB } from "@/util/mongodb";
+import MainNav from "./MainNav";
+import { getUserCookies } from "@/util/getUserCookie";
 
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
-import logo from "@/public/icons/icon-nav-logo.svg";
-import menu from "@/public/icons/icon-menu.svg";
-import { usePathname, useRouter } from "next/navigation";
-import Link from "next/link";
-import SubNav from "./SubNav";
-import useUserStore from "@/store/useUserStore";
+const Nav = async () => {
+  const cookie = await getUserCookies();
+  const userId = await cookie?.userId;
 
-const Nav = () => {
-  const router = useRouter();
-  const pathName = usePathname();
-  const [isShowMenu, setIsShowMenu] = useState(false);
-  const { userId } = useUserStore();
-  const [isAdmin, setIsAdmin] = useState<boolean | null>(null);
+  const db = (await connectDB).db("donson");
 
-  useEffect(() => {
-    if (!userId) return;
-
-    const verifyAdmin = async () => {
-      const res = await fetch("/api/verify-admin", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userId }),
-      });
-      const result = await res.json();
-      setIsAdmin(res.ok && result === true);
-    };
-    verifyAdmin();
-  }, [userId]);
-
-  useEffect(() => {
-    console.log(userId);
-  }, [userId]);
-
-  if (
-    pathName === "/signin" ||
-    pathName === "/signup" ||
-    pathName === "/find/id" ||
-    pathName === "/find/password"
-  ) {
-    return null;
+  // 유저 DP 포인트
+  const targetDP = await db.collection("dp").findOne({ userId });
+  if (!targetDP) {
+    await db
+      .collection("dp")
+      .insertOne({ userId, dp: 0, updatedAt: Date.now() });
   }
+  const dp = targetDP?.dp;
+
+  // 관리자 계정 확인
+  const targetAdmin = await db.collection("user").findOne({ userId });
+  const isAdmin = (await targetAdmin?.admin) === true ? true : false;
 
   return (
-    <nav className="fixed top-0 left-0 w-full h-14 px-3 flex justify-between items-center border-b border-[#bebebe] bg-white">
-      <ul className="flex items-center gap-4 text-xs">
-        <Image
-          onClick={() => {
-            router.push("/");
-          }}
-          className="border border-[#bebebe] rounded-full cursor-pointer"
-          src={logo}
-          alt="돈슨 로고"
-          width={30}
-          priority
-        />
-        <li
-          style={{ fontWeight: pathName === "/info" ? "bold" : "" }}
-          className="cursor-pointer"
-        >
-          <Link href="/info">캐릭터 정보</Link>
-        </li>
-        <li
-          style={{ fontWeight: pathName === "/notice" ? "bold" : "" }}
-          className="cursor-pointer"
-        >
-          <Link href="/notice">공지사항</Link>
-        </li>
-        <li
-          style={{ fontWeight: pathName === "/event" ? "bold" : "" }}
-          className="cursor-pointer"
-        >
-          <Link href="/event">이벤트</Link>
-        </li>
-        <div className="relative">
-          <li
-            style={{ fontWeight: pathName === "/cash" ? "bold" : "" }}
-            className="cursor-pointer"
-          >
-            <Link href="/cash">캐시샵 공지</Link>
-          </li>
-          <span className="absolute top-0 right-0 translate-x-2/3 -translate-y-2/3 w-3 h-3 flex justify-center items-center text-[10px] text-white bg-red-500 rounded-full">
-            N
-          </span>
-        </div>
-      </ul>
-      <button
-        onClick={() => {
-          setIsShowMenu(true);
-        }}
-      >
-        <Image src={menu} alt="메뉴 아이콘" width={20} priority />
-      </button>
-      {isShowMenu && (
-        <SubNav
-          setIsShowMenu={setIsShowMenu}
-          isAnimating={isShowMenu}
-          isAdmin={isAdmin}
-        />
-      )}
-    </nav>
+    <section>
+      <MainNav dp={dp} isAdmin={isAdmin} />
+    </section>
   );
 };
 
