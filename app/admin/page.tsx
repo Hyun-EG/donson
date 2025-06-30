b47bbb30a551840711878a3e3621aee28076bae6
seonghyun <codiee@naver.com>
2025-07-01 00:42:33 +0900

    Feat: 객체 데이터 배열로 가공 및 인계

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,9 @@
 import { connectDB } from "@/util/mongodb";
-import { Contact } from "./_components/types";
-import AdminBox from "./_components/AdminBox";
-import MaplePointPriceForm from "./_components/MaplePointPriceForm";
 import { getUserCookies } from "@/util/getUserCookie";
 import { redirect } from "next/navigation";
+import MaplePointPriceForm from "./_components/MaplePointPriceForm";
+import AdminBox from "./_components/AdminBox";
+import { Contact } from "./_components/types";
 
 const Admin = async () => {
   const cookie = await getUserCookies();
@@ -13,6 +13,7 @@ const Admin = async () => {
 
   const db = (await connectDB).db("donson");
   const rawContact = await db.collection("contact").find().toArray();
+  const rawOrders = await db.collection("ordered-items").find().toArray();
 
   const contact: Contact[] = rawContact.map((doc) => ({
     _id: doc._id.toString(),
@@ -21,11 +22,20 @@ const Admin = async () => {
     content: doc.content,
   }));
 
+  const groupedOrders = rawOrders.reduce((acc, curr) => {
+    if (!acc[curr.userId]) acc[curr.userId] = [];
+    acc[curr.userId].push({
+      title: curr.title,
+      done: curr.done,
+    });
+    return acc;
+  }, {} as Record<string, { title: string; done: boolean }[]>);
+
   return (
-    <section>
+    <section className="mb-10">
       <h1 className="text-lg text-center font-bold">관리자 메뉴</h1>
       <MaplePointPriceForm />
-      <AdminBox contacts={contact} />
+      <AdminBox contacts={contact} groupedOrders={groupedOrders} />
     </section>
   );
 };
