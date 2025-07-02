4d8ef04562836b8208575805c24d17fc79ba6ae0
Hyun-EG <codiee@naver.com>
2025-07-02 16:46:36 +0900

    Feat: 주문내역 처리상태, 삭제버튼 추가 및 api 연결

--- app/mypage/_components/MyPageBox.tsx
+++ app/mypage/_components/MyPageBox.tsx
@@ -12,13 +12,13 @@ const MyPageBox = ({
   userId,
   userName,
   userEmail,
-  resOrderedItems,
+  resBoughtItems,
 }: {
   charName: string;
   userId: string;
   userName: string;
   userEmail: string;
-  resOrderedItems: OrderedItemsType[];
+  resBoughtItems: OrderedItemsType[];
 }) => {
   const router = useRouter();
 
@@ -34,6 +34,15 @@ const MyPageBox = ({
   const [password, setPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmNewPassword, setConfirmNewPassword] = useState("");
+
+  const formatDate = (timestamp: number) => {
+    if (!timestamp || isNaN(timestamp)) return "날짜 없음";
+    const date = new Date(timestamp);
+    if (isNaN(date.getTime())) return "날짜 오류";
+    return date.toISOString().slice(0, 10);
+  };
+
+  console.log(resBoughtItems.map((i) => i.boughtAt));
 
   const handleEditProfile = async () => {
     setIsLoading(true);
@@ -105,6 +114,34 @@ const MyPageBox = ({
       return res;
     } catch (error) {
       console.error("비밀번호 변경 중 에러가 발생했습니다.", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleDeleteCompleteItem = async (title: string) => {
+    setIsLoading(true);
+    try {
+      const res = await fetch("/api/delete-bought-item", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId,
+          title,
+        }),
+      });
+
+      if (!res.ok) {
+        alert("상품 삭제에 실패하였습니다.");
+        return;
+      } else {
+        alert("처리완료 상품이 삭제되었습니다.");
+      }
+      router.refresh();
+    } catch (_) {
+      alert("상품 삭제 처리중 에러가 발생하였습니다.");
     } finally {
       setIsLoading(false);
     }
@@ -228,36 +265,67 @@ const MyPageBox = ({
       </div>
       <section className="mt-2 flex flex-col gap-1">
         <h1 className="mb-2 font-bold text-center">주문내역</h1>
-        <p className="text-xs font-bold">
-          * 상품 처리완료 시 해당 아이템박스는 사라집니다.
+        <p className="text-xs font-bold">중복거래방지</p>
+        <p className="text-xs text-red-500 font-bold">
+          *{" "}
+          <span className="underline">
+            처리완료된 상품 삭제시에만 해당 상품 재 구매가 가능합니다.
+          </span>
         </p>
-        {resOrderedItems.map((item) => (
-          <article
-            key={item.title}
-            className="px-2 py-1 border border-[#bebebe] rounded-[6px]"
-          >
-            <p className="text-xs">
-              상품명: <span className="font-bold">{item.title}</span>
-            </p>
-            <p className="text-xs">
-              가격: <span className="font-bold">{item.dpPoint}</span>
-            </p>
-            <p className="text-xs">
-              상품내용:{" "}
-              <span className="text-xs font-bold">{item.description}</span>
-            </p>
-            <p className="text-xs">
-              처리상태:{" "}
-              <span
-                className={`font-bold ${
-                  item.done ? "text-sky-500" : "text-red-500"
-                }`}
-              >
-                {item.done ? "처리 완료" : "처리 중"}
-              </span>
-            </p>
-          </article>
-        ))}
+        <p className="text-xs font-bold">
+          * 삭제버튼은 처리완료 상품에만 노출됩니다.
+        </p>
+        {resBoughtItems && resBoughtItems.length > 0 ? (
+          resBoughtItems.map((item) => (
+            <article
+              key={item._id}
+              className="px-2 py-1 border border-[#bebebe] rounded-[6px]"
+            >
+              <p className="text-xs">
+                상품명: <span className="font-bold">{item.title}</span>
+              </p>
+              <p className="text-xs">
+                가격: <span className="font-bold">{item.dpPoint}</span>
+              </p>
+              <p className="text-xs">
+                상품내용:{" "}
+                <span className="text-xs font-bold">{item.description}</span>
+              </p>
+              <p className="text-xs">
+                구매날짜:{" "}
+                <span className="text-xs font-bold">
+                  {formatDate(item.boughtAt)}
+                </span>
+              </p>
+              <div className="flex justify-between items-center">
+                <p className="text-xs">
+                  처리상태:{" "}
+                  <span
+                    className={`font-bold ${
+                      item.done ? "text-sky-500" : "text-red-500"
+                    }`}
+                  >
+                    {item.done ? "처리 완료" : "처리 중"}
+                  </span>
+                </p>
+                {item.done && (
+                  <button
+                    onClick={() => {
+                      handleDeleteCompleteItem(item.title);
+                    }}
+                    className="px-2 py-1 mr-4 text-xs text-white bg-sky-500 rounded-[6px]"
+                  >
+                    삭제하기
+                  </button>
+                )}
+              </div>
+            </article>
+          ))
+        ) : (
+          <p className="py-4 text-center text-sm font-bold">
+            주문 내역이 없습니다.
+          </p>
+        )}
       </section>
     </section>
   );
