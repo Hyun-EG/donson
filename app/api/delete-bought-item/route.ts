753199fa6e3154bcebe1dc203b9bd2f02c1dd5de
Hyun-EG <codiee@naver.com>
2025-07-02 16:47:33 +0900

    Feat: 구매한 아이템 삭제 api 추가

---
+++ app/api/delete-bought-item/route.ts
@@ -0,0 +1,20 @@
+import { connectDB } from "@/util/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { userId, title } = await req.json();
+
+  try {
+    const db = (await connectDB).db("donson");
+    await db.collection("bought-items").deleteOne({ userId, title });
+    return NextResponse.json(
+      { message: "처리된 상품 삭제를 완료했습니다.", status: 200 },
+      { status: 200 }
+    );
+  } catch (_) {
+    return NextResponse.json(
+      { message: "삭제 처리중 에러가 발생하였습니다", status: 500 },
+      { status: 500 }
+    );
+  }
+}
