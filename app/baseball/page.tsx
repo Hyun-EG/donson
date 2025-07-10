490bf44dfa8bbd582efc17cbf146e77043b2102b
seonghyun <codiee@naver.com>
2025-07-10 22:13:57 +0900

    Feat: 토큰 없을 시 로그인으로 페이지 렌더시 기존 게임 삭제

--- app/baseball/page.tsx
+++ app/baseball/page.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-import BaseBallBox from "./BaseBallBox";
+import BaseBallBox from "./_components/BaseBallBox";
+import { getUserCookies } from "@/util/getUserCookie";
+import { redirect } from "next/navigation";
+import { connectDB } from "@/util/mongodb";
 
-const BaseBall = () => {
+const BaseBall = async () => {
+  const cookies = await getUserCookies();
+  const userId = await cookies?.userId;
+
+  const db = (await connectDB).db("donson");
+
+  if (!userId || typeof userId !== "string") {
+    redirect("/signin");
+  }
+
+  await db.collection("baseball").deleteOne({ userId });
+
   return (
     <section>
       <h1 className="text-center text-lg font-bold">야구게임</h1>
-      <BaseBallBox />
+      <BaseBallBox userId={userId} />
     </section>
   );
 };
