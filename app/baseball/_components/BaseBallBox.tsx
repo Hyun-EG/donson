39d19c86d56c6d9e1ce62998c085a26d073e23b4
seonghyun <codiee@naver.com>
2025-07-10 23:33:35 +0900

    Style: 글꼴 색깔 수정

--- app/baseball/_components/BaseBallBox.tsx
+++ app/baseball/_components/BaseBallBox.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const BaseBallBox = ({ userId }: { userId: string }) => {
   const [isStart, setIsStart] = useState(false);
@@ -81,8 +81,17 @@ const BaseBallBox = ({ userId }: { userId: string }) => {
     <section className="w-full px-2 py-1 mt-2 border">
       {!isStart && (
         <div>
+          <div className="my-4">
+            <p className="text-sm text-center font-bold">
+              승리 시 [<span className="text-sky-500"> 0.1dp </span>] 를
+              적립합니다.
+            </p>
+            <p className="text-center text-red-500 font-bold">
+              게임 횟수: 무제한
+            </p>
+          </div>
           <ul>
-            <li className="text-sm">RULE</li>
+            <li className="text-sm font-bold">RULE</li>
             <li className="text-xs">- 4자리의 임의의 숫자가 주어집니다</li>
             <li className="text-xs">
               - 주어진 4자리의 숫자와 자릿수까지 맞추면 게임에 승리합니다.
@@ -101,9 +110,11 @@ const BaseBallBox = ({ userId }: { userId: string }) => {
             <li className="text-xs text-red-500">
               - 중복된 숫자는 존재하지 않습니다.
             </li>
-            <li>ex. 정답: 1340 제출한 답: 1438</li>
+            <li className="mt-4">ex. 정답: 1340 제출한 답: 1438</li>
             <li>
-              <span>STRIKE</span>: 1 <span>BALL</span>: 2 <span>OUT</span>: 1
+              <span className="text-sky-500 font-bold">STRIKE</span>: 1
+              <span className="text-orange-500 font-bold"> BALL</span>: 2{" "}
+              <span className="text-red-500 font-bold">OUT</span>: 1
             </li>
           </ul>
           <div className="my-10 flex justify-center">
@@ -121,44 +132,54 @@ const BaseBallBox = ({ userId }: { userId: string }) => {
         <div className="w-full h-96 pt-4 flex flex-col justify-between">
           <div>
             <p className="text-sky-500 text-center">게임이 시작됐습니다.</p>
-            <p>
+            <p className="text-sm">
               추론기회: <span className="text-red-500">{10 - turn}</span>
             </p>
-            <p>
+            <p className="text-sm">
               제출기회: <span className="text-red-500">1</span>
             </p>
           </div>
           <div className="text-sm">
             {history.map((h, idx) => (
               <p key={idx}>
-                {idx + 1}. <span>{h.guess}</span> STRIKE:{" "}
-                <span>{h.strike}</span> BALL: <span>{h.ball}</span> OUT:{" "}
-                <span>{h.out}</span>
+                {idx + 1}. <span className="font-bold">{h.guess} 결과: </span>{" "}
+                <span className="text-sky-500 font-bold">{h.strike}</span>
+                <span className="text-sky-500"> STRIKE </span>
+                <span className="text-orange-500 font-bold">{h.ball}</span>
+                <span className="text-orange-500"> BALL </span>
+                <span className="text-red-500 font-bold">{h.out}</span>
+                <span className="text-red-500"> OUT</span>
               </p>
             ))}
           </div>
-          <div className="w-full flex flex-col items-center">
-            <div>
+          <div className="w-full flex flex-col items-center gap-1">
+            <div className="flex gap-1">
               <input
                 type="number"
                 value={guessInput}
                 onChange={(e) => setGuessInput(e.target.value)}
-                className="pl-2 border"
+                className="pl-2 py-1 text-sm border rounded-[6px]"
                 placeholder="숫자를 입력해주세요."
               />
-              <button onClick={handleGuess} className="px-2 border">
+              <button
+                onClick={handleGuess}
+                className="px-2 py-1 border rounded-[6px]"
+              >
                 추론
               </button>
             </div>
-            <div>
+            <div className="flex gap-1">
               <input
                 type="number"
                 value={answerInput}
                 onChange={(e) => setAnswerInput(e.target.value)}
-                className="pl-2 border"
+                className="pl-2 py-1 text-sm border rounded-[6px]"
                 placeholder="정답을 입력해주세요."
               />
-              <button onClick={handleSubmit} className="px-2 border">
+              <button
+                onClick={handleSubmit}
+                className="px-2 py-1 border rounded-[6px]"
+              >
                 제출
               </button>
             </div>
