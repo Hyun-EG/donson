cabfaa572d337776d809ae7462825d0f81d30cc6
Hyun-EG <codiee@naver.com>
2025-07-13 19:16:43 +0900

    Feat: 리프레시토큰 추가 액세스토큰 직관적인 변수명으로 변경

--- util/jwt.ts
+++ util/jwt.ts
@@ -1,18 +1,23 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 
-const SECRET = process.env.JWT_SECRET!;
+const SECRET_ACCESS = process.env.ACCESS_SECRET!;
+const REFRESH_ACCESS = process.env.REFRESH_SECRET!;
 
 export interface MyJwtPayload extends JwtPayload {
   ocid?: string;
 }
 
-export const signJWT = (payload: MyJwtPayload) => {
-  return jwt.sign(payload, SECRET, { expiresIn: "1h" });
+export const accessToken = (payload: MyJwtPayload) => {
+  return jwt.sign(payload, SECRET_ACCESS, { expiresIn: "1h" });
+};
+
+export const refreshToken = (payload: MyJwtPayload) => {
+  return jwt.sign(payload, REFRESH_ACCESS, { expiresIn: "30d" });
 };
 
 export const verifyJWT = (token: string) => {
   try {
-    return jwt.verify(token, SECRET) as MyJwtPayload;
+    return jwt.verify(token, SECRET_ACCESS) as MyJwtPayload;
   } catch (error) {
     return null;
   }
