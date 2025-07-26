6762608297691e615a87685492ac4cd61ee89348
Hyun-EG <codiee@naver.com>
2025-07-26 19:35:30 +0900

    Feat: 선택된 네브 아이템에 렌더여부 정함

--- app/admin/_components/AdminBox.tsx
+++ app/admin/_components/AdminBox.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import { Contact } from "./types";
 import InquiryDetailsBox from "./InquiryDetailsBox";
 import OrderedBox from "./OrderedBox";
+import AdminNav from "./AdminNav";
+import MaplePointPriceForm from "./MaplePointPriceForm";
+import PostAlertToUserBox from "./PostAlertToUserBox";
+import { useAdminNavStore } from "@/store/adminNavStore";
+import SearchCustomer from "./SearchCustomer";
 
 const AdminBox = ({
   contacts,
@@ -9,10 +16,19 @@ const AdminBox = ({
   contacts: Contact[];
   groupedOrders: Record<string, { title: string; done: boolean }[]>;
 }) => {
+  const { selectedNavItem } = useAdminNavStore();
   return (
     <section>
-      <InquiryDetailsBox contacts={contacts} />
-      <OrderedBox groupedOrders={groupedOrders} />
+      <AdminNav />
+      {selectedNavItem === "메포반영" && <MaplePointPriceForm />}
+      {selectedNavItem === "알림전송" && <PostAlertToUserBox />}
+      {selectedNavItem === "문의내역" && (
+        <InquiryDetailsBox contacts={contacts} />
+      )}
+      {selectedNavItem === "주문내역" && (
+        <OrderedBox groupedOrders={groupedOrders} />
+      )}
+      {selectedNavItem === "고객검색" && <SearchCustomer />}
     </section>
   );
 };
