6b34baf6614a5df988b8b51757bd85cf4d815d9e
seonghyun <codiee@naver.com>
2025-07-26 15:11:48 +0900

    Docs: 상품설명 추가

--- app/loan/page.tsx
+++ app/loan/page.tsx
@@ -6,8 +6,12 @@ const LoanPage = () => {
     <main>
       <h1 className="text-lg font-bold text-center">디피론</h1>
       <ul className="text-sm">
-        <li>- DP를 대출해드립니다</li>
-        <li>- 이자는 없습니다</li>
+        <li>- DP를 빌려드립니다!</li>
+        <li>- 이자는 없습니다!</li>
+        <li className="text-xs text-red-500">
+          - 상환이 완료되지 않은 경우, DP 스토어에서 상품 주문이 거절될 수
+          있습니다
+        </li>
       </ul>
       <LoanBox />
     </main>
